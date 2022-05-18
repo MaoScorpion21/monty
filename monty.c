43d48644efba6c9a9f67422ab457c7c788fd3fa9
Leyneryiret <4704@holbertonschool.com>
2022-05-18 18:33:49 -0500

    monty.c

--- monty.c
+++ monty.c
@@ -8,19 +8,43 @@
 int main(int argn, char *argv[])
 {
   FILE *archivoM = NULL;
-  size_t linea = 0;
+  unsigned int line_num = 1;
   int read = 0;
-  char *buffer = NULL, *archivo = NULL;
+  size_t linea = 0;
+  char *buffer = NULL, *archivo = NULL, *opcion_codigo = NULL, *numero = NULL;
+
+  if (argn != 2)
+    printf ("USAGE: monty file");
 
   archivo = argv[1];
-  archivoM = fopen(archivo, "r");
-  if (!archivoM)
+  if (access(archivo, R_OK) == EOF)
     {
-      fprintf(stderr, "Error: Can't open file %s\n", archiv);
+      fprintf(stderr, "Error: Can't open file %s\n", archivo);
       exit(EXIT_FAILURE);
     }
-  read = getline(&buffer, &linea, archivoM);
-  while (read != EOF)
+  archivoM = fopen(archivo, "r");
+  if (!archivoM)
+     {
+      fprintf(stderr, "Error: Can't open file %s\n", archivo);
+      exit(EXIT_FAILURE);
+    }
+  while ((read = getline(&buffer, &linea, archivoM)) != EOF)
+    {
+      opcion_codigo = strtok(buffer, "\t\n ");
+      if (opcion_codigo)
+	{
 
+	  if (opcion_codigo[0] == '$')
+	    {
+	      ++line_num;
+	      continue;
+	    }
+	  numero = strtok(NULL, "\t\n ");
+	  printf ("cod = %s, parametro %d , linea= %d \n" , opcion_codigo, atoi(numero), line_num);
+	}
 
+      line_num++;
+    }
+  free(buffer);
+  return (0);
 }
