e4dc76b6ff157724e617b3bfd91aaf740d9b7b12
Leyneryiret <4704@holbertonschool.com>
2022-05-19 23:18:45 -0500

    update

--- add.c
+++ add.c
@@ -7,18 +7,18 @@
  */
 void add(stack_t **h, unsigned int line_num)
 {
-    stack_t *aux = *h;
-    int new = 0;
+	stack_t *aux = *h;
+	int new = 0;
 
-    if ((*h) == NULL || (*h)->next == NULL)
-    {
-        fprintf(stderr, "L%u: can't add, stack too short\n", line_num);
-        exit(EXIT_FAILURE);
-    }
+	if ((*h) == NULL || (*h)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", line_num);
+		exit(EXIT_FAILURE);
+	}
 
-    new = aux->n + aux->next->n;
-    (*h) = aux->next;
-    aux->next->n = new;
-    if (aux->next)
-        aux->next->prev = NULL;
+	new = aux->n + aux->next->n;
+	(*h) = aux->next;
+	aux->next->n = new;
+	if (aux->next)
+		aux->next->prev = NULL;
 }
