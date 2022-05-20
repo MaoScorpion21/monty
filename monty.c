797a9c8d238158ae503d84af314ca715c63e6bb2
MaoScorpion21 <mao7021@hotmail.com>
2022-05-20 10:57:47 -0500

    Fixing errors

--- monty.c
+++ monty.c
@@ -4,7 +4,7 @@ tool tools = {NULL, NULL, NULL, NULL};
  * main - Program that interprets bytecode (.m) and execute opcode
  * @argn: number of arguments.
  * @argv: Arguments passed to the program.
- * Return: 0 on success or exit with code 1.
+ * Return: always 0 and on error EXIT_FAILURE
  **/
 int main(int argn, char *argv[])
 {
@@ -28,7 +28,7 @@ int main(int argn, char *argv[])
 	while ((read = getline(&tools.buffer, &linea, tools.archivoM)) != EOF)
 	{
 		toquenizar(tools.buffer);
-		if (tools.comando != NULL && tools.comando[0] != '$')
+		if (tools.comando != NULL && tools.comando[0] != '#')
 			instruccion(line_num, tools.comando, &head);
 		line_num++;
 	}
